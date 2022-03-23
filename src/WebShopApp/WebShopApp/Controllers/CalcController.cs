8119a796d99a4dbef70cfc9fcd6abd139a7ea04e
Vanya <Vanya.120101@yandex.ru>
2022-03-23 22:41:25 +0300

    Added calc

--- src/WebShopApp/WebShopApp/Controllers/CalcController.cs
+++ src/WebShopApp/WebShopApp/Controllers/CalcController.cs
@@ -4,9 +4,9 @@ namespace WebShopApp.Controllers
 {
 	public class CalcController : Controller
 	{
-		public int Add(int a, int b)
+		public int Add(int x, int y)
 		{
-			return a + b;
+			return x + y;
 		}
 
 		public int Mul(int a, int b)
@@ -22,6 +22,33 @@ namespace WebShopApp.Controllers
 		public int Sub(int a, int b)
 		{
 			return a - b;
+		}
+
+		public int Use(int x, int y, string operation)
+		{
+			if(operation == "+")
+			{
+				return Add(x, y);
+			}
+			if(operation == "-")
+			{
+				return Sub(x, y);
+			}
+			if(operation == "*")
+			{
+				return Mul(x, y);
+			}
+			if(operation == "/")
+			{
+				return Div(x, y);
+			}
+			return 0;
+		}
+
+
+		public IActionResult ViewCalc()
+		{
+			return View("Show");
 		}
 	}
 }
--- src/WebShopApp/WebShopApp/Controllers/HomeController.cs
+++ src/WebShopApp/WebShopApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using WebShopApp.Models;
 
 namespace WebShopApp.Controllers
 {
@@ -16,6 +18,29 @@ namespace WebShopApp.Controllers
 			string path = Path.Combine(Environment.CurrentDirectory, "wwwroot", "001. Introduction.pdf");
 			byte[] data = System.IO.File.ReadAllBytes(path);
 			return File(data, "application/octet-stream", "lesson.pdf");
+		}
+
+		public IActionResult Cars()
+		{
+			var cars = new List<Car>();
+			cars.Add(new Car { Id = 0, Name = "JuJu", Price = 555 });
+			cars.Add(new Car { Id = 0, Name = "JuJu", Price = 555 });
+			cars.Add(new Car { Id = 0, Name = "JuJu", Price = 555 });
+			cars.Add(new Car { Id = 0, Name = "JuJu", Price = 555 });
+			cars.Add(new Car { Id = 0, Name = "JuJu", Price = 555 });
+
+			return View(cars);
+		}
+
+		public IActionResult Info()
+		{
+			foreach(var item in Request.Headers)
+			{
+				var str = item.ToString();
+			}
+			ViewBag.Ip = Request.HttpContext.Connection.LocalIpAddress;
+			ViewBag.Browser = Request.Headers["User-Agent"].ToString();
+			return View();
 		}
 	}
 }
